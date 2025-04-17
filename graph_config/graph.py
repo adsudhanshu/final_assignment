46da4ca6321ec7f78b95a7b7b383c9760689c80b
adsudhanshu <adsudhanshu@deloitte.com>
2025-04-18 02:44:09 +0530

    Creating Zip of the Generated FastAPI project

--- graph_config/graph.py
+++ graph_config/graph.py
@@ -5,6 +5,9 @@ from tools.project_setup import project_setup_chain
 from graph_config.model import GraphState
 from tools.generate_unit_test import generate_unit_tests
 from tools.generate_backend_code import generate_backend_code
+from tools.automated_validation import automated_validation_node
+from tools.deployment import ZipGeneratedProjectNode
+from persistence.crud import save_graph_state
 
 def build_analysis_graph():
     graph = StateGraph(GraphState)
@@ -17,9 +20,19 @@ def build_analysis_graph():
     graph.add_node("generate_unit_test",generate_unit_tests)
 
     graph.add_node("generate_backend_code",generate_backend_code)
+
+    graph.add_node("automated_validation",automated_validation_node)
+
+    persistence_node = RunnableLambda(save_graph_state)
+    graph.add_node("PersistentGraphState",persistence_node)
+
+    zip_node = ZipGeneratedProjectNode(zip_filename='generated_project')
+    graph.add_node("ZipGeneratedProject", zip_node.execute)
+
     graph.set_entry_point("AnalyzeSRS")
     graph.add_edge("AnalyzeSRS","ProjectSetup")
     graph.add_edge("ProjectSetup","generate_unit_test")
     graph.add_edge("generate_unit_test","generate_backend_code")
     graph.add_edge("generate_backend_code",END)
+    # graph.add_edge("automated_validation",END)
     return graph.compile()
---
+++ tools/deployment.py
@@ -0,0 +1,20 @@
+import shutil
+import os
+
+# Define the zip functionality
+class ZipGeneratedProjectNode:
+    def __init__(self, zip_filename='generated_project'):
+        self.zip_filename = zip_filename
+    
+    def execute(self):
+        # Navigate to the root directory from the 'tools' directory
+        project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  # Go up one level
+        
+        # Path to the generated project directory (relative path from root)
+        generated_project_path = os.path.join(project_root, 'generated_project')
+        
+        # Create the zip file in the root folder, excluding 'generated_project' from the zip path
+        shutil.make_archive(self.zip_filename, 'zip', project_root, 'generated_project')
+        
+        print(f"Project zipped to: {self.zip_filename}.zip")
+        return f"{self.zip_filename}.zip"  
